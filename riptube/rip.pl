bcf57bc904903ef41c1e66d856f36040ee7cd47d
Capaverde <dudu_capaverde@terra.com.br>
2018-07-07 21:49:18 -0300

    a commit

--- riptube/rip.pl
+++ riptube/rip.pl
@@ -1,14 +1,15 @@
-#use: perl rip.pl <vid id>
+#use: perl rip.pl <vid id> <filename>
+#e.g. perl rip.pl gOqblSqx_VI pigs.mp4
+
 my $id = $ARGV[0];
+my $filename = $ARGV[1];
 my $json = `curl "https://hooktube.com/api?mode=video&id=$id"`;
 my $url;
 
-while ($json =~ /"18":\["([^"]+)"/g){ 
-$url = "$1";
+if ($json =~ /"18":\["([^"]+)"/){ #18 - 640x360 mp4
+	$url = "$1";
 }
 
 $url =~ s/\\\//\//g;
 
-my $redirect = `curl "$url"`;
-
-print "$redirect";
+`curl -L "$url" > $filename`;
